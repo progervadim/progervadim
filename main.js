a0984badc255b1df2dbd2056b21144379cb11746
progervadim <63308037+progervadim@users.noreply.github.com>
2020-04-19 21:40:32 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+$(function () {
+  var Grid = function (width, height) {};
+  const cartButton = document.querySelector("#cart-button");
+  const modal = document.querySelector(".modal");
+  const close = document.querySelector(".close");
+  const remove = document.querySelector(".remove");
+  
+  cartButton.addEventListener("click", function (event) {
+    modal.classList.add("is-open");
+  });
+    close.addEventListener("click", function (event) {
+      modal.classList.remove("is-open");
+  });
+  remove.addEventListener("click", function (event) {
+    modal.classList.remove("is-open");
+});
+  
+  
+});
